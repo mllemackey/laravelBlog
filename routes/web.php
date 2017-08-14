2829f03b18fa0abec3f412b672138d9f81939298
Vivify Academy <mllemackey@gmail.com>
2017-08-14 19:35:08 +0200

    Vezba 1 - Register users

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@
 Route::get('/posts', 'PostsController@index');
 Route::get('/posts/create', 'PostsController@create');
 Route::get('/posts/{id}', 'PostsController@show');
+Route::get('/register', 'RegisterController@create');
+Route::post('/register', 'RegisterController@store');
 Route::post('/posts', 'PostsController@store');
 
 Route::post('/posts/{id}/comment', 'CommentsController@store');
